333371ab2f12e8652c1277ef7f4371820fb60626
joannef3188 <joannef3188@hstat.org>
2017-03-09 05:05:41 +0000

    forismatic api works

--- models/model.rb
+++ models/model.rb
@@ -1,8 +1,19 @@
-require 'net/http'
-require 'json'
-require 'pp'
+# require 'net/http'
+# require 'json'
+# require 'pp'
  
-url = 'http://api.forismatic.com/api/1.0/'
-uri = URI(url)
-response = Net::HTTP.get(uri)
+# url = 'http://api.forismatic.com/api/1.0/'
+# uri = URI(url)
+# response = Net::HTTP.get(uri)
 # JSON.parse(response)
+
+# http://api.forismatic.com/api/1.0/
+
+require "net/http"
+require "uri"
+
+uri = URI.parse("http://api.forismatic.com/api/1.0/")
+
+response = Net::HTTP.post_form(uri, {"method" => "getQuote", "key" => "457653", "format" => "json", "lang" => "en"})
+
+puts response.body
