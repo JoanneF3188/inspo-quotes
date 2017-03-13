49698ec20acb9afc49f55c47035f49f96db1d711
joannef3188 <joannef3188@hstat.org>
2017-03-13 18:25:48 +0000

    edit quote class

--- models/model.rb
+++ models/model.rb
@@ -4,16 +4,18 @@ require "net/http"
 require "uri"
 require "pp"
 
-uri = URI.parse("http://api.forismatic.com/api/1.0/")
 
-response = Net::HTTP.post_form(uri, {"method" => "getQuote", "key" => "457653", "format" => "json", "lang" => "en"})
 
-hash = response.body
-
-@quote = eval(hash).first[1]
-
-puts @quote
-
-# class Get_quote
+class Quote
+    attr_reader :quote
     
-# end
+    def initialize
+        uri = URI.parse("http://api.forismatic.com/api/1.0/")
+        response = Net::HTTP.post_form(uri, {"method" => "getQuote", "key" => "457653", "format" => "json", "lang" => "en"})
+        hash = eval(response.body)
+        @quote = hash
+    end
+    
+    
+
+end
