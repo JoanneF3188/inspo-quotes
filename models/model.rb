05b54dd152c74fb1c52016b5e90ed07e04f48a0e
kelleyl9631 <kelleyl9631@hstat.org>
2017-03-16 18:48:54 +0000

    Add button and fix merges

--- models/model.rb
+++ models/model.rb
@@ -6,7 +6,7 @@ require "uri"
 class Quote
     attr_reader :quote, :feeling
     
-    def initialize(feeling)
+    def initialize(*feeling)
         uri = URI.parse("http://api.forismatic.com/api/1.0/")
         response = Net::HTTP.post_form(uri, {"method" => "getQuote", "key" => "457653", "format" => "json", "lang" => "en"})
         hash = eval(response.body)
@@ -22,10 +22,9 @@ class Quote
     end
     
     def match_quote
-    
              match_quote = params[@quote]
-        end
-    
     end
+    
+end
 
 
