68389c18cdae926d6b1840d7b3308d3b444d1923
justing5321 <justing5321@hstat.org>
2017-03-16 16:15:30 +0000

    error

--- models/model.rb
+++ models/model.rb
@@ -10,10 +10,7 @@ class Quote
         uri = URI.parse("http://api.forismatic.com/api/1.0/")
         response = Net::HTTP.post_form(uri, {"method" => "getQuote", "key" => "457653", "format" => "json", "lang" => "en"})
         hash = eval(response.body)
-
-        @quote = hash
-        
-
+         @quote = hash
         @quote = hash.first[1]
         puts @quote
 
@@ -21,6 +18,13 @@ class Quote
     
     def get_quote
         @quote
+    end
+    
+    # def match_quote
+    #     until @quote ==  do | |
+    #          match_quote = params[@quote]
+    #     end
+    
     end
 
 end
